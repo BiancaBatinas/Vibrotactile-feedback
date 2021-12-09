ec1f05d1da36a32d306d59effea0257eefe46756
BiancaBatinas <biancabatinas64@gmail.com>
2021-12-09 13:15:39 +0200

    main
    da

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Vireo from './Components/Patterns/Vibratie1';
 function App() {
+  const Vib =()=> {
+    if ('vibrate' in window.navigator)
+    window.navigator.vibrate(100);
+else
+    console.log("pa");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button> DA</button>
         <a
           className="App-link"
           href="https://reactjs.org"
